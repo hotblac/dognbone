74ec14f5b26921d872fd174cb9fea5ff84431260
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2018-12-18 10:52:40 +0000

    Login form validation

--- src/LoginModal.js
+++ src/LoginModal.js
@@ -3,6 +3,50 @@ import 'bulma/css/bulma.css'
 
 export class LoginModal extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            accountSid: '',
+            accountSidValidationError: '',
+            authToken: '',
+            authTokenValidationError: ''
+        };
+    }
+
+    /**
+     * Validates all form input and sets validation error messages as required.
+     * @returns {boolean} true iff form is valid
+     */
+    validateForm = () => {
+        let accountSidValidationError = '';
+        let authTokenValidationError = '';
+        if (!this.state.accountSid) {
+            accountSidValidationError = 'Account SID is required';
+        }
+
+        if (!this.state.authToken) {
+            authTokenValidationError = 'Auth Token is required';
+        }
+
+        this.setState({
+            accountSidValidationError: accountSidValidationError,
+            authTokenValidationError: authTokenValidationError
+        });
+
+        return !(accountSidValidationError || authTokenValidationError);
+    };
+
+    handleInputChange = (event) => {
+        const target = event.target;
+        this.setState({
+            [target.name]: target.value
+        });
+    };
+
+    handleSubmit = () => {
+        this.validateForm();
+    };
+
     render() {
         return (
             <div className={'modal ' + (this.props.visible ? 'is-active' : '')}>
@@ -13,25 +57,39 @@ export class LoginModal extends Component {
                     </header>
                     <section className="modal-card-body">
                         <p className="level">Login with your Twilio Account SID.</p>
-                        <div className="field is-horizontal">
+                        <div id="accountSidField" className="field is-horizontal">
                             <label className="field-label">Account SID</label>
                             <div className="field-body">
-                                    <div className="field control is-expanded">
-                                        <input className="input" type="text"/>
+                                <div className="field is-expanded">
+                                    <div className="control">
+                                        <input name="accountSid" type="text"
+                                               onChange={this.handleInputChange}
+                                               className={'input ' + (this.state.accountSidValidationError ? 'is-danger' : '')}/>
                                     </div>
+                                    {this.state.accountSidValidationError &&
+                                    <p className="help is-danger">{this.state.accountSidValidationError}</p>
+                                    }
                                 </div>
+                            </div>
                         </div>
-                        <div className="field is-horizontal">
+                        <div id="authTokenField" className="field is-horizontal">
                             <label className="field-label">Auth Token</label>
                             <div className="field-body">
-                                <div className="field control is-expanded">
-                                    <input className="input" type="password"/>
+                                <div className="field is-expanded">
+                                    <div className="control">
+                                        <input name="authToken" type="password"
+                                               onChange={this.handleInputChange}
+                                               className={'input ' + (this.state.authTokenValidationError ? 'is-danger' : '')}/>
+                                    </div>
+                                    {this.state.authTokenValidationError &&
+                                    <p className="help is-danger">{this.state.authTokenValidationError}</p>
+                                    }
                                 </div>
                             </div>
                         </div>
                     </section>
                     <footer className="modal-card-foot">
-                        <button className="button is-success">Login</button>
+                        <button className="button is-success" onClick={this.handleSubmit}>Login</button>
                     </footer>
                 </div>
             </div>
--- src/LoginModal.test.js
+++ src/LoginModal.test.js
@@ -16,3 +16,82 @@ describe('login modal', () => {
         expect(modal.hasClass('is-active')).toBe(false);
     });
 });
+
+describe('form validation', () => {
+
+    const validAccountSid = 'ACffffffffffffffffffffffffffffffff';
+    const validAuthToken = 'ffffffffffffffffffffffffffffffff';
+
+    it('should show no errors on initial form', () => {
+        const wrapper = shallow(<LoginModal/>);
+
+        const accountSidInput = wrapper.find('#accountSidField input');
+        const accountSidValidationHelp = wrapper.find('#accountSidField .help');
+        const authTokenInput = wrapper.find('#authTokenField input');
+        const authTokenValidationHelp = wrapper.find('#authTokenField .help');
+
+        expect(accountSidInput.hasClass('is-danger')).toBe(false);
+        expect(authTokenInput.hasClass('is-danger')).toBe(false);
+        expect(accountSidValidationHelp.exists()).toBe(false);
+        expect(authTokenValidationHelp.exists()).toBe(false);
+    });
+
+    it('should show error on blank account SID', () => {
+        const wrapper = shallow(<LoginModal/>);
+        submitForm(wrapper, '',validAuthToken);
+
+        const accountSidInput = wrapper.find('#accountSidField input');
+        const accountSidValidationHelp = wrapper.find('#accountSidField .help');
+        expect(accountSidInput.hasClass('is-danger')).toBe(true);
+        expect(accountSidValidationHelp.exists()).toBe(true);
+    });
+
+    it('should show error on blank auth token', () => {
+        const wrapper = shallow(<LoginModal/>);
+        submitForm(wrapper, validAccountSid,'');
+
+        const authTokenInput = wrapper.find('#authTokenField input');
+        const authTokenValidationHelp = wrapper.find('#authTokenField .help');
+        expect(authTokenInput.hasClass('is-danger')).toBe(true);
+        expect(authTokenValidationHelp.exists()).toBe(true);
+    });
+
+    it('should show no errors on valid input', () => {
+        const wrapper = shallow(<LoginModal/>);
+        submitForm(wrapper, validAccountSid,validAuthToken);
+
+        const accountSidInput = wrapper.find('#accountSidField input');
+        const accountSidValidationHelp = wrapper.find('#accountSidField .help');
+        const authTokenInput = wrapper.find('#authTokenField input');
+        const authTokenValidationHelp = wrapper.find('#authTokenField .help');
+
+        expect(accountSidInput.hasClass('is-danger')).toBe(false);
+        expect(authTokenInput.hasClass('is-danger')).toBe(false);
+        expect(accountSidValidationHelp.exists()).toBe(false);
+        expect(authTokenValidationHelp.exists()).toBe(false);
+    });
+});
+
+function submitForm(wrapper, accountSid, authToken) {
+    const accountSidInput = wrapper.find('#accountSidField input');
+    const authTokenInput = wrapper.find('#authTokenField input');
+    const button = wrapper.find('button');
+
+    accountSidInput.simulate('change', stubEvent(accountSidInput, accountSid));
+    authTokenInput.simulate('change', stubEvent(authTokenInput, authToken));
+    button.simulate('click');
+}
+
+/**
+ * Stubbed event object suitable for event simulation.
+ * As we use shallow rendering, we don't get real DOM events.
+ * This assumes that we use the named input handler pattern:
+ * @link https://reactjs.org/docs/forms.html#handling-multiple-inputs
+ */
+function stubEvent(node, value) {
+    const name = node.prop('name');
+    return {target: {
+            name: name,
+            value: value
+        }};
+}
